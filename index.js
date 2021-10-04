c2c5de0c3304403c254ba3124a3ba0276f55b1aa
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-10-04 16:42:34 -0500

    Bug fixes

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 
 const { prompt } = require("inquirer");
-const logo = require("asciiart-logo");
 const db = require("./db");
-require("console.table");
 
 init();
 
 function init() {
-    const logoText = logo({ name: "Employee Manager" }).render();
   
-    console.log(logoText);
   
     loadMainPrompts();
   }
